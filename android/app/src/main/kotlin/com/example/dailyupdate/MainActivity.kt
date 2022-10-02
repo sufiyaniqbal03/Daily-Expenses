b93e4e315e2203d2ff03a020274eb3a81a4d1d21
Sufiyan Iqbal <sufiyaniqbal2103@gmail.com>
2022-10-02 13:04:59 +0530

    first commit

---
+++ android/app/src/main/kotlin/com/example/dailyupdate/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.dailyupdate
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/widgets/new_transaction.dart
@@ -0,0 +1,103 @@
+import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
+
+class NewTransaction extends StatefulWidget {
+  final Function addTx;
+
+  NewTransaction(this.addTx);
+
+  @override
+  _NewTransactionState createState() => _NewTransactionState();
+}
+
+class _NewTransactionState extends State<NewTransaction> {
+  final _titleController = TextEditingController();
+  DateTime  _selectedDate = DateTime.now();
+  final _amountController = TextEditingController();
+
+  void _submitData() {
+    if(_amountController.text.isEmpty) return;
+    final enteredTitle = _titleController.text;
+    final enteredAmount = double.parse(_amountController.text);
+
+    if (enteredTitle.isEmpty || enteredAmount <= 0 || _selectedDate == null) {
+      return;
+    }
+
+    widget.addTx(
+      enteredTitle,
+      enteredAmount,
+      _selectedDate,
+    );
+
+    Navigator.of(context).pop();
+  }
+  void _presentDatePicker()
+  {
+    showDatePicker(
+        context: context,
+        initialDate: DateTime.now(),
+        firstDate: DateTime(2021),
+        lastDate: DateTime.now()
+    ).then((pickeddate) {
+      if(pickeddate == null)
+        return;
+      _selectedDate = pickeddate;
+    },);
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return SingleChildScrollView(
+      child: Card(
+        elevation: 5,
+        child: Container(
+          padding: EdgeInsets.only(top:10,
+          left: 10,
+          right: 10,
+          bottom: MediaQuery.of(context).viewInsets.bottom + 10,
+          ),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.end,
+            children: <Widget>[
+              TextField(
+                decoration: InputDecoration(labelText: 'Title'),
+                controller: _titleController,
+                onSubmitted: (_) => _submitData(),
+                // onChanged: (val) {
+                //   titleInput = val;
+                // },
+              ),
+              TextField(
+                decoration: InputDecoration(labelText: 'Amount'),
+                controller: _amountController,
+                keyboardType: TextInputType.number,
+                onSubmitted: (_) => _submitData(),
+                // onChanged: (val) => amountInput = val,
+              ),
+             Container(
+               height: 70,
+               child: Row(children: <Widget> [
+                 Expanded(
+                   child: Text(_selectedDate == DateTime.now() ?'No Date Choosen!':'Picked Date : ${DateFormat.yMd().format(_selectedDate)}',
+                   ),
+                 ),
+                 OutlinedButton(
+                   child: Text('Choose Date',
+                     style: TextStyle(fontWeight: FontWeight.bold),
+                   ),
+                   onPressed: _presentDatePicker,
+                 ),
+               ],),
+             ),
+              ElevatedButton(
+                child: Text('Add Transaction'),
+                onPressed: _submitData,
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
